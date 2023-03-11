1481625fa150537877897545901e9ef5f822d3b9
Vlad B <belousov7vlad@gmail.com>
2023-03-11 18:37:37 +0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,11 @@ import "./Footer.sass";
 import logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
 
+import inst from '../../assets/inst.png'
+import tlg from '../../assets/tlg.png'
+import wa from '../../assets/wa.png'
+import vk from '../../assets/vk.png'
+
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -12,12 +17,22 @@ export const Footer = () => {
         <NavLink>Контакты</NavLink>
       </div>
       <div className="footer__contacts">
-        <a href="" className="footer__contacts-tel">
+        <a href="tel:2222222222222222222222222222222222222" className="footer__contacts-tel">
           тел. +7 (921) 656 57 61
         </a>
-        <a href="" className="footer__contacts-adress">
+        <address className="footer__contacts-adress">
           Россия, г Санкт-Петербург, ул. Ольги Берггольц, д 36, стр 2, офис 1
-        </a>
+        </address>
+        <div className="footer__contacts-socials">
+          <p className="footer__contacts-socials-text">Свяжитесь с нами:</p>
+          <div className="footer__contacts-socials-list">
+            <a href="#" className="footer__contacts-socials-link"><img src={inst} alt="" /></a>
+            <a href="#" className="footer__contacts-socials-link"><img src={tlg} alt="" /></a>
+            <a href="#" className="footer__contacts-socials-link"><img src={wa} alt="" /></a>
+            <a href="#" className="footer__contacts-socials-link "><img src={vk} alt="" /></a>
+          </div>
+
+        </div>
       </div>
     </footer>
   );
