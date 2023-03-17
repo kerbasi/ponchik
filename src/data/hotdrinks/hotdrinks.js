d14ccfc1576b6488ee45ca6e5cc2a7533d29f814
Ruslan Gaziev <grinrus@yandex.ru>
2023-03-17 15:25:07 +0300

    База для напитков

---
+++ src/data/hotdrinks/hotdrinks.js
@@ -0,0 +1,27 @@
+import hotdrinks_1 from './images/hot-drink-1.jpg';
+import hotdrinks_2 from './images/hot-drink-2.jpg';
+import hotdrinks_3 from './images/hot-drink-3.jpg';
+import hotdrinks_4 from './images/hot-drink-4.jpg';
+import hotdrinks_5 from './images/hot-drink-5.jpg';
+import hotdrinks_6 from './images/hot-drink-6.jpg';
+import hotdrinks_7 from './images/hot-drink-7.jpg';
+import hotdrinks_8 from './images/hot-drink-8.jpg';
+import hotdrinks_9 from './images/hot-drink-9.jpg';
+
+export const HOT_DRINKS = [
+  {
+    id: 'hot-drink-1',
+    image: hotdrinks_1,
+    title: 'Пирожок с картошкой',
+    sizes: [
+      {
+        size: 'L',
+        price: 200,
+      },
+      {
+        size: 'XL',
+        price: 300,
+      },
+    ],
+  },
+];
