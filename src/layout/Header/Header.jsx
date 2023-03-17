2157986cff6f6b07b8d2857c005ba8625a75e47e
Vlad B <belousov7vlad@gmail.com>
2023-03-17 15:20:42 +0300

    logo container

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,25 +1,27 @@
-import './Header.sass';
-import { Navigation } from '../../Components/Navigation/Navigation.jsx';
-import logo from '../../assets/logo.svg';
-import { NavLink } from 'react-router-dom';
-import { Select } from '../../Components/Select/Select';
+import "./Header.sass";
+import { Navigation } from "../../Components/Navigation/Navigation.jsx";
+import logo from "../../assets/logo.svg";
+import { NavLink } from "react-router-dom";
+import { Select } from "../../Components/Select/Select";
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
-      <NavLink to="/" className="header__logo-nav">
-        <img src={logo} alt="Логотип" />
-      </NavLink>
+        <div className="header__logo-container">
+          <NavLink to="/" className="header__logo-nav">
+            <img src={logo} alt="Логотип" />
+          </NavLink>
+        </div>
         <Navigation />
 
-      <div className="header__icons">
-        <Select />
-        <button
-          className="header__cart"
-          onClick={() => alert('Переход в корзину')}
-        />
-      </div>
+        <div className="header__icons">
+          <Select />
+          <button
+            className="header__cart"
+            onClick={() => alert("Переход в корзину")}
+          />
+        </div>
       </div>
     </header>
   );
