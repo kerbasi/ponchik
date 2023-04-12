4cf177f7a33f1d9616d6f6f9b05b06325693fd48
Kirill <kirill.novikov@gmial.com>
2023-04-12 21:24:30 +0300

    add adaptive to fotter nav

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,29 +1,29 @@
-import './Navigation.sass';
-import { NavLink } from 'react-router-dom';
+import "./Navigation.sass";
+import { NavLink } from "react-router-dom";
 
-export const Navigation = () => {
+export const Navigation = ({ footer }) => {
   return (
-    <nav className="nav">
+    <nav className={footer ? "nav nav_type_footer" : "nav"}>
       <NavLink
-        to="/menu/donuts"
+        to='/menu/donuts'
         className={({ isActive }) =>
-          isActive ? 'nav__link nav__link_active' : 'nav__link'
+          isActive ? "nav__link nav__link_active" : "nav__link"
         }
       >
         Меню
       </NavLink>
       <NavLink
-        to="/about"
+        to='/about'
         className={({ isActive }) =>
-          isActive ? 'nav__link nav__link_active' : 'nav__link'
+          isActive ? "nav__link nav__link_active" : "nav__link"
         }
       >
         О нас
       </NavLink>
       <NavLink
-        to="/contacts"
+        to='/contacts'
         className={({ isActive }) =>
-          isActive ? 'nav__link nav__link_active' : 'nav__link'
+          isActive ? "nav__link nav__link_active" : "nav__link"
         }
       >
         Контакты
--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -8,35 +8,35 @@ import vk from "../../assets/vk.png";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <img src={logo} alt="" className="footer__logo" />
-      <div className="footer__nav">
-        <Navigation />
+    <footer className='footer'>
+      <img src={logo} alt='' className='footer__logo' />
+      <div className='footer__nav'>
+        <Navigation footer='true' />
       </div>
-      <div className="footer__contacts">
+      <div className='footer__contacts'>
         <a
-          href="tel:2222222222222222222222222222222222222"
-          className="footer__contacts-tel"
+          href='tel:2222222222222222222222222222222222222'
+          className='footer__contacts-tel'
         >
           тел. +7 (921) 656 57 61
         </a>
-        <address className="footer__contacts-adress">
+        <address className='footer__contacts-adress'>
           Россия, г Санкт-Петербург, ул. Ольги Берггольц, д 36, стр 2, офис 1
         </address>
-        <div className="footer__contacts-socials">
-          <p className="footer__contacts-socials-text">Свяжитесь с нами:</p>
-          <div className="footer__contacts-socials-list">
-            <a href="#" className="footer__contacts-socials-link">
-              <img src={inst} alt="" />
+        <div className='footer__contacts-socials'>
+          <p className='footer__contacts-socials-text'>Свяжитесь с нами:</p>
+          <div className='footer__contacts-socials-list'>
+            <a href='#' className='footer__contacts-socials-link'>
+              <img src={inst} alt='' />
             </a>
-            <a href="#" className="footer__contacts-socials-link">
-              <img src={tlg} alt="" />
+            <a href='#' className='footer__contacts-socials-link'>
+              <img src={tlg} alt='' />
             </a>
-            <a href="#" className="footer__contacts-socials-link">
-              <img src={wa} alt="" />
+            <a href='#' className='footer__contacts-socials-link'>
+              <img src={wa} alt='' />
             </a>
-            <a href="#" className="footer__contacts-socials-link ">
-              <img src={vk} alt="" />
+            <a href='#' className='footer__contacts-socials-link '>
+              <img src={vk} alt='' />
             </a>
           </div>
         </div>
