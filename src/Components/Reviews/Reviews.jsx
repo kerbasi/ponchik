e35cc2d4a730f78fe1869c7fa17bb26920689329
Kirill <kirill.novikov@gmial.com>
2023-04-13 00:38:04 +0300

    Add slider, need to fix buttons

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,15 +1,34 @@
-import './Reviews.sass';
-import { ReviewCard } from './ReviewCard/ReviewCard';
-import { REVIEWS_DATA } from '../../data/reviews/reviews';
+import "./Reviews.sass";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { ReviewCard } from "./ReviewCard/ReviewCard";
+import { REVIEWS_DATA } from "../../data/reviews/reviews";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper";
 
 export const Reviews = () => {
   return (
-    <section className="reviews">
-      <h2 className="reviews__title">Отзывы</h2>
+    <section className='reviews'>
+      <h2 className='reviews__title'>Отзывы</h2>
       <div className='reviews__cards'>
-        {REVIEWS_DATA.map((item) => (
-          <ReviewCard key={item.id} review={item} />
-        ))}
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          className='mySwiper'
+        >
+          {REVIEWS_DATA.map((item) => (
+            <SwiperSlide>
+              <ReviewCard key={item.id} review={item} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
