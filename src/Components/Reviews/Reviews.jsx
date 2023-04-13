527da7aad386008285f48602a46cadcbde72fdd5
Kirill <kirill.novikov@gmail.com>
2023-04-13 14:13:51 +0300

    Switch to swiper element

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,34 +1,70 @@
 import "./Reviews.sass";
-import { Swiper, SwiperSlide } from "swiper/react";
 
 import { ReviewCard } from "./ReviewCard/ReviewCard";
 import { REVIEWS_DATA } from "../../data/reviews/reviews";
 
-import "swiper/css";
-import "swiper/css/navigation";
+import { useRef, useEffect } from "react";
+// import function to register Swiper custom elements
+import { register } from "swiper/element/bundle";
+// register Swiper custom elements
+register();
 
-import { Navigation } from "swiper";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
+
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
 export const Reviews = () => {
+  const swiperElRef = useRef(null);
+
+  useEffect(() => {
+    const swiperContainer = swiperElRef.current;
+    const params = {
+      navigation: true,
+      pagination: true,
+
+      injectStyles: [
+        `
+          .swiper-button-next,
+          .swiper-button-prev {
+            background-color: white;
+            padding: 8px 16px;
+            border-radius: 100%;
+            border: 2px solid black;
+            color: red;
+          }
+          .swiper-pagination-bullet{
+            width: 40px;
+            height: 40px;
+            background-color: red;
+          }
+      `,
+      ],
+    };
+
+    Object.assign(swiperContainer, params);
+    swiperContainer.initialize();
+  }, []);
+
   return (
     <section className='reviews'>
       <h2 className='reviews__title'>Отзывы</h2>
       <div className='reviews__cards'>
-        <Swiper
-          navigation={true}
-          modules={[Navigation]}
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-          className='mySwiper'
+        <swiper-container
+          ref={swiperElRef}
+          slides-per-view='3'
+          speed='500'
+          loop='true'
+          navigation='true'
         >
           {REVIEWS_DATA.map((item) => (
-            <SwiperSlide>
+            <swiper-slide>
               <ReviewCard key={item.id} review={item} />
-            </SwiperSlide>
+            </swiper-slide>
           ))}
-        </Swiper>
+        </swiper-container>
       </div>
     </section>
   );
