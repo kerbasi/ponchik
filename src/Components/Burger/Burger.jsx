1efd31655b20362ac2e25487ddd5ae674bccfd9c
Kirill <kirill.novikov@gmail.com>
2023-04-19 17:43:33 +0300

    add coursor pinter

--- src/Components/Burger/Burger.jsx
+++ src/Components/Burger/Burger.jsx
@@ -3,13 +3,14 @@ import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
 import { useState } from "react";
 
 export const Burger = ({ handleBurgerBtnClick, isBurgerActive, isMobile }) => {
-  const className = isBurgerActive
-    ? "burger-nav__btn burger-nav__btn_active"
-    : "burger-nav__btn";
-
   return (
     <>
-      <div className={className} onClick={handleBurgerBtnClick}>
+      <div
+        className={`burger-nav__btn ${
+          isBurgerActive ? "burger-nav__btn_active" : ""
+        }`}
+        onClick={handleBurgerBtnClick}
+      >
         <span />
         <BurgerMenu isActive={isBurgerActive} />
       </div>
